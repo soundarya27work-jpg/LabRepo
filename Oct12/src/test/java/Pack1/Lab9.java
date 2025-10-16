152000f4b5a9c3b7b3c32d5e863d9fd17fbd082d
ASC23-SOUNDARYA-GIT-HUB <soundarya27work@gmail.com>
2025-10-16 23:21:37 +0530

    Add files via upload

---
+++ Oct12/src/test/java/Pack1/Lab9.java
@@ -0,0 +1,93 @@
+package Pack1;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class Lab9 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void setUp() {
+        String browser = System.getProperty("browser", "chrome");
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("ie")) {
+            driver = new InternetExplorerDriver();
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.get("https://tutorialsninja.com/demo/index.php");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testCompleteFlow() {
+        // Step 1: 
+        String expectedTitle = "Your Store";
+        wait.until(ExpectedConditions.titleIs(expectedTitle));
+        Assert.assertEquals(expectedTitle, driver.getTitle());
+
+        // Step2: 
+        WebElement desktopsMenu = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Desktops")));
+        desktopsMenu.click();
+
+        WebElement macLink = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Mac (1)")));
+        macLink.click();
+
+        // Step: 3 
+        WebElement macHeading = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Mac']")));
+        Assert.assertEquals("Mac", macHeading.getText());
+
+        // Step 4: 
+        WebElement sortElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("input-sort")));
+        Select sortDropdown = new Select(sortElement);
+        sortDropdown.selectByVisibleText("Name (A - Z)");
+
+        // Step 5: 
+        WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//button[@onclick[contains(., 'cart.add')]]")));
+        addToCartButton.click();
+
+        // Step 6: 
+        WebElement searchBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("search")));
+        searchBox.clear();
+        searchBox.sendKeys("Monitors");
+
+        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.cssSelector("button[class='btn btn-default btn-lg']")));
+        searchButton.click();
+
+        // Step 7: 
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("content")));
+
+        // Step 8: 
+        WebElement searchCriteria = driver.findElement(By.name("search"));
+        searchCriteria.clear();
+
+        // Step:9 
+        WebElement descriptionCheckbox = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("description")));
+        if (!descriptionCheckbox.isSelected()) {
+            descriptionCheckbox.click();
+        }
+
+        WebElement searchAgainButton = wait.until(ExpectedConditions.elementToBeClickable(
+            By.cssSelector("input[type='button'][value='Search']")));
+        searchAgainButton.click();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
